3fe463e5d4a47a9e8c4a5b7e427e7dd61406e6d3
Shocam7 <87086200+Shocam7@users.noreply.github.com>
2025-06-24 18:38:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { MobileNav } from "@/components/mobile-nav"
+import { AuthProvider } from "@/hooks/use-auth"
+import { Toaster } from "sonner"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,10 +24,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <div className="relative min-h-screen flex flex-col">
-            {children}
-            <MobileNav />
-          </div>
+          <AuthProvider>
+            <div className="relative min-h-screen flex flex-col">
+              {children}
+              <MobileNav />
+            </div>
+            <Toaster position="top-right" />
+          </AuthProvider>
         </ThemeProvider>
       </body>
     </html>
